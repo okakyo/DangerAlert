e3ccb2d8daa547d31d602e8e0f675dc565287f8e
Kyohei Oka <0622okakyo@gmail.com>
2019-02-24 17:38:18 +0900

    'angular使い方確認'

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  News = [
+    'マリオ',
+    'リンク',
+    'カービィ',
+    'ピカチュウ',
+    'サムス',
+  ]
+}
