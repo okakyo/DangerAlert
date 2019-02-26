298de513c4cf59d8033d2ae05a6412447b8f1569
Kyohei Oka <0622okakyo@gmail.com>
2019-02-27 03:40:06 +0900

    'Leafletで実装'

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,6 @@
 import { Component, OnInit  } from '@angular/core';
 import { Platform } from '@ionic/angular';
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  Marker,
-  LocationService,
-  GoogleMapsAnimation,
-  MyLocation,
-  GoogleMapOptions
-} from '@ionic-native/google-maps';
+import { Map, latLng, tileLayer, marker } from 'leaflet';
 @Component({
 
   selector: 'app-tab1',
@@ -18,7 +9,7 @@ import {
 })
 
 export class Tab1Page implements OnInit {
-  map: GoogleMap;
+  map: Map;
   
   // このHomePageクラスが作成されるときに実行される
   constructor(private platform: Platform){
@@ -34,56 +25,10 @@ export class Tab1Page implements OnInit {
   }
 
   async loadMap() {
-    LocationService.getMyLocation().then((location: MyLocation)=>{
-    let options: GoogleMapOptions={
-      camera: {
-        target: location.latLng,
-        zoom: 17,
-        tilt: 30
-      }
-    }
-    // Googleマップを作成
-    this.map = GoogleMaps.create('map_canvas',options);
-
-    let marker: Marker = this.map.addMarkerSync({
-      position: location.latLng,
-      animation: GoogleMapsAnimation.BOUNCE
-    });
-
-    // 情報ウィンドウの表示
-    marker.showInfoWindow();
-  })
 }
 
   // ボタンが押された時の処理
   onButtonClick() {
 
     // 現在位置を取得
-    this.map.getMyLocation().then((location: MyLocation) => {
-      // アニメーションで指定の位置にズームイン 
-      this.map.animateCamera({
-        target: location.latLng,
-        zoom: 17,
-        tilt: 30
-      });
-
-      // アニメーションが終了したらマーカーを追加
-      let marker: Marker = this.map.addMarkerSync({
-        position: location.latLng,
-        animation: GoogleMapsAnimation.BOUNCE
-      });
-
-      // 情報ウィンドウの表示
-      marker.showInfoWindow();
-
-      // もし情報ウィンドウがクリックされたら、アラートを表示
-      marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-        alert('clicked!');
-      })
-    })
-    .catch(err => {
-      // getMyLocationでエラーが発生したら、メッセージを表示
-      alert(err.error_message);
-    });
-  }
 }
