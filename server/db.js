e3756cab6acf1fafa1e66ecc04d08c1e3661992c
Kyohei Oka <0622okakyo@gmail.com>
2019-02-26 21:41:09 +0900

    'データベース実装中'

--- server/db.js
+++ server/db.js
@@ -7,3 +7,4 @@ var db=mysql.createConnection({
     'password':''
 });
 
+
---
+++ server/router.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const PORT=1243;
+
+app=express();
+
+app.get('/',()=>{});
+app.get('/tab',()=>{});
+app.get()
+app.listen(PORT,()=>{});
