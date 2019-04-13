ccb09d6a8fdcc445f8118f39c4e28466a3a74665
Kyohei Oka <0622okakyo@gmail.com>
2019-04-14 01:12:58 +0900

    '変更'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import {GoogleAnalytics} from '@ionic-native/google-analytics/ngx';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -12,16 +12,16 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private ga:GoogleAnalytics,
+    
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-     
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      
     });
   }
 }
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import {HttpClientModule,HttpClientJsonpModule} from '@angular/common/http';
-
+import {GoogleAnalytics} from '@ionic-native/google-analytics/ngx';
 
 @NgModule({
   imports: [
@@ -14,7 +14,7 @@ import {HttpClientModule,HttpClientJsonpModule} from '@angular/common/http';
     FormsModule,
     HttpClientModule,
     HttpClientJsonpModule,
-    
+    GoogleAnalytics,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
   declarations: [Tab1Page]
