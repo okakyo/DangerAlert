55d0d176e51c2eb5310842a546ee0ce910a47236
Kyohei Oka <0622okakyo@gmail.com>
2019-04-14 02:06:23 +0900

    'いろいろ変更'

---
+++ src/app/ga.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+declare let gtag:any;
+@Injectable({
+  providedIn: 'root'
+})
+export class GaService {
+
+  constructor() { }
+
+  private useGA():boolean{
+    return typeof gtag!==undefined;
+  }
+  sendPageView(url:string):void{
+    if(!this.useGA()){return;}
+    gtag('config','UA-137860602-1',{
+      'page-path':url
+    });
+  }
+  sendEvent(eventName:string,eventCategory:string,eventAction:string,eventLabel:any):void{
+    if(!this.useGA){
+      return;    }
+    gtag('event',eventName,{
+      event_category:eventCategory,
+      event_action:eventAction,
+      event_label:eventLabel
+    });
+  }
+}
--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -9,15 +9,30 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: '../tab1/tab1.module#Tab1PageModule'
+        children: [
+          {
+            path: '',
+            loadChildren: '../tab1/tab1.module#Tab1PageModule'
+          }
+        ]
       },
       {
         path: 'tab2',
-        loadChildren: '../tab2/tab2.module#Tab2PageModule'
+        children: [
+          {
+            path: '',
+            loadChildren: '../tab2/tab2.module#Tab2PageModule'
+          }
+        ]
       },
       {
         path: 'tab3',
-        loadChildren: '../tab3/tab3.module#Tab3PageModule'
+        children: [
+          {
+            path: '',
+            loadChildren: '../tab3/tab3.module#Tab3PageModule'
+          }
+        ]
       },
       {
         path: '',
